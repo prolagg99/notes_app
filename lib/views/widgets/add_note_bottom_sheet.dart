e3d981f3784847458c32c929f76c2db026eb6678
lagg99 <pro.lagg99@gmail.com>
2024-10-13 16:58:42 +0100

    add border raidus to bottom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -5,6 +5,8 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Column(
+      children: [],
+    );
   }
 }
