e0d13c2a9d88be62fc2e05c4aa655c7f8f71dfc0
lagg99 <pro.lagg99@gmail.com>
2024-10-13 18:56:29 +0100

    create edit note view

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class NoteApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowMaterialGrid: false,
       theme: ThemeData(
         brightness: Brightness.dark,
         fontFamily: 'Poppins',
--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/views/widgets/custom_note_item.dart';
+import 'package:notes_app/views/widgets/note_item.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
